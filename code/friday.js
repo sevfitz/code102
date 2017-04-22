1aafaceb487f9240a669d19129901b5ec698e5c9
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-04-21 20:31:39 -0700

    Refactor all the code into one function, dayApp()

--- code/friday.js
+++ code/friday.js
@@ -17,84 +17,29 @@
 
 // TODO: Refactor the if/elseif/else block that comments
 //       on each day into this function:
-function remarkOnDay(day) {
-
-}
-
-// var day = 'Monday',
-//     thursday = 'thursday';
+// function remarkOnDay(day) {
 //
-// // How many equals signs does it take to screw your code?
-// // if (day = 'thursday') {
-// //   console.log('Test 1: Well lookee there, it\'s ' + day);
-// // }
-// //
-// // // The case of the failing "if" statement
-// // if (day == 'Thursday') {
-// //   console.log('Test 2: Lo and behold, it\'s ' + day);
-// // }
-// //
-// // // Equals thrice, test once
-// // if (day === 'Thursday') {
-// //   console.log('Test 3: Huh. It\'s ' + day);
-// // }
-//
-// //TODO: After commenting out the 3 conditionals above,
-// //      uncomment this code and run it.
-// if (day === 'Thursday') {
-//   console.log('Is it Friday yet?');
-// } else if (day === 'Saturday' || day === 'Sunday') {
-//   console.log('Enjoy the day!');
-// } else if (day === 'Monday') {
-//   console.log('Uggh. Another Monday');
-// } else {
-//   console.log('Whistle while you work!')
 // }
 
-// TODO: Add an "else if" block to check whether it's Saturday
-//       and console.log() a different statement for the weekend.
-
-
-
-// TODO: Modify the conditional for Saturday to check whether
-//       the day is Saturday OR Sunday.
-
-
-
-// TODO: Add an "else if" block to check whether it's Monday
-//       and console.log() a different statement for Monday.
-
-
-
-// TODO: Change the string that's assigned into the day
-//       variable and run the code several times.
-
-
-
+var day = 'Monday';
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var max = 5;
 
-// console.log('index 0: ' + days[0]);
-// console.log('index 1: ' + days[1]);
-// console.log('index 2: ' + days[2]);
-// console.log('index 6: ' + days[6]);
-// console.log('index 7: ' + days[7]);
-// console.log('index 20: ' + days[20]);
+function dayApp() {
 
-// TODO: DRY it up!
-var max = days.length,
-    result;
-for (var j = 0; j < max; j++) {
-  dayNumber = Math.floor(Math.random() * days.length);
+  if (day === 'Thursday') {
+    console.log('Is it Friday yet?');
+  } else if (day === 'Saturday' || day === 'Sunday') {
+    console.log('Enjoy the weekend!');
+  } else if (day === 'Monday') {
+    console.log('Uggh. Another Monday');
+  } else {
+    console.log('Whistle while you work!')
+  }
+
+  for (var j = 0; j < days.length; j++) {
+    dayNumber = Math.floor(Math.random() * days.length);
+    console.log('index ' + dayNumber + ': ' + days[dayNumber]);
+  }
 }
-
-for (var i = 0; i < days.length; i++) {
-  console.log('index ' + dayNumber + ': ' + days[dayNumber]);
-}
-
-
-// TODO: Replace the "7" in "i < 7" with the length of the array
-
-
-// TODO: On each iteration, use our randomly generated dayNumber
-//       to get a day from the days array, and console.log the
-//       name of the day.
+dayApp();
