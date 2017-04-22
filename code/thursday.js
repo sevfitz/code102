3af3b583e15b73d38056ea97a8976d50773a530d
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-04-21 18:51:19 -0700

    Change line 1 of Readme on Friday

--- code/thursday.js
+++ code/thursday.js
@@ -1,25 +1,29 @@
-var day = 'Thursday',
-    thursday = 'thursday';
-
-// How many equals signs does it take to screw your code?
-if (day = 'thursday') {
-  console.log('Test 1: Well lookee there, it\'s ' + day);
-}
-
-// The case of the failing "if" statement
-if (day == 'Thursday') {
-  console.log('Test 2: Lo and behold, it\'s ' + day);
-}
-
-// Equals thrice, test once
-if (day === 'Thursday') {
-  console.log('Test 3: Huh. It\'s ' + day);
-}
-
-// TODO: After commenting out the 3 conditionals above,
-//       uncomment this code and run it.
+// var day = 'Monday',
+//     thursday = 'thursday';
+//
+// // How many equals signs does it take to screw your code?
+// // if (day = 'thursday') {
+// //   console.log('Test 1: Well lookee there, it\'s ' + day);
+// // }
+// //
+// // // The case of the failing "if" statement
+// // if (day == 'Thursday') {
+// //   console.log('Test 2: Lo and behold, it\'s ' + day);
+// // }
+// //
+// // // Equals thrice, test once
+// // if (day === 'Thursday') {
+// //   console.log('Test 3: Huh. It\'s ' + day);
+// // }
+//
+// //TODO: After commenting out the 3 conditionals above,
+// //      uncomment this code and run it.
 // if (day === 'Thursday') {
 //   console.log('Is it Friday yet?');
+// } else if (day === 'Saturday' || day === 'Sunday') {
+//   console.log('Enjoy the day!');
+// } else if (day === 'Monday') {
+//   console.log('Uggh. Another Monday');
 // } else {
 //   console.log('Whistle while you work!')
 // }
@@ -54,9 +58,15 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // console.log('index 20: ' + days[20]);
 
 // TODO: DRY it up!
-// for (var i = 0; i < 7; i++) {
-//   console.log('index ' + i + ': ' + days[i]);
-// }
+var max = days.length,
+    result;
+for (var j = 0; j < max; j++) {
+  dayNumber = Math.floor(Math.random() * days.length);
+}
+
+for (var i = 0; i < days.length; i++) {
+  console.log('index ' + dayNumber + ': ' + days[dayNumber]);
+}
 
 
 // TODO: Replace the "7" in "i < 7" with the length of the array
@@ -65,8 +75,3 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 // TODO: On each iteration, use our randomly generated dayNumber
 //       to get a day from the days array, and console.log the
 //       name of the day.
-// var max = 100,
-//     result;
-// for (var j = 0; j < max; j++) {
-//   dayNumber = Math.floor(Math.random() * days.length);
-// }
